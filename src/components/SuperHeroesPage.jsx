84c58ce788926293f49f4ebedab39629ef96e126
shivprajapat <shivkumarprajapat232@gmail.com>
2023-01-29 11:26:53 +0530

    Query Error

--- src/components/SuperHeroesPage.jsx
+++ src/components/SuperHeroesPage.jsx
@@ -4,26 +4,33 @@ import React, { useEffect, useState } from "react";
 const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((response) => {
-      setData(response.data);
-      setIsLoading(false)
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
   console.log("data", data);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <h2>{error}</h2>;
+  }
   return (
     <div>
       <h1>Super Heroes Page</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {data.map((item, i) => (
-            <p key={i}>{item.name}</p>
-          ))}
-        </>
-      )}
+      {data.map((item, i) => (
+        <p key={i}>{item.name}</p>
+      ))}
     </div>
   );
 };
--- src/query/SuperHeroesPage.jsx
+++ src/query/SuperHeroesPage.jsx
@@ -3,22 +3,23 @@ import React from "react";
 import { useQuery } from "react-query";
 
 const SuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("superheroes", () => {
-    return axios.get("http://localhost:4000/superheroes");
+  const { isLoading, data, isError, error } = useQuery("superheroes", () => {
+    return axios.get("http://localhost:4000/superheroe");
   });
+
   console.log("data", data);
+  if (isLoading) {
+    return <p>Loading.....</p>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
   return (
     <div>
       <h1>React Query Super Heroes Page</h1>
-      {isLoading ? (
-        <p>Loading.....</p>
-      ) : (
-        <>
-          {data.data.map((item, i) => (
-            <p key={i}>{item.name}</p>
-          ))}
-        </>
-      )}
+      {data.data.map((item, i) => (
+        <p key={i}>{item.name}</p>
+      ))}
     </div>
   );
 };
